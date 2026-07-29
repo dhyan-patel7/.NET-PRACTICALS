37b7dd0f7b18f8ca223a13397cebb384fb18d5d6
dhyan-patel7 <dhyanmakadia1000000@gmail.com>
2026-07-29 19:38:20 +0530

    Add files via upload

---
+++ PR1/PR1/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR1
+{
+    // Student Class
+    public class Student
+    {
+        private int number;
+        private string name;
+        private string course;
+        private int semester;
+        private double fees;
+
+        // Constructor
+        public Student(int num, string n, string c, int sem, double f)
+        {
+            number = num;
+            name = n;
+            course = c;
+            semester = sem;
+            fees = f;
+        }
+
+        // Scholarship Method
+        public void Scholarship()
+        {
+            double percentage;
+
+            Console.Write("\nEnter Student Percentage: ");
+            percentage = Convert.ToDouble(Console.ReadLine());
+
+            if (percentage >= 75)
+            {
+                Console.WriteLine("Congratulations! Student is eligible for Scholarship.");
+
+                // 20% fee discount
+                fees = fees - (fees * 20 / 100);
+
+                Console.WriteLine("Updated Fees after Scholarship: " + fees);
+            }
+            else
+            {
+                Console.WriteLine("Student is NOT eligible for Scholarship.");
+                Console.WriteLine("Fees remains: " + fees);
+            }
+        }
+
+        // Display Method
+        public void Display()
+        {
+            Console.WriteLine("\n------ Student Admission Details ------");
+            Console.WriteLine("Student Number : " + number);
+            Console.WriteLine("Student Name   : " + name);
+            Console.WriteLine("Course         : " + course);
+            Console.WriteLine("Semester       : " + semester);
+            Console.WriteLine("Fees           : " + fees);
+        }
+    }
+
+    // Main Class
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number, semester;
+            string name, course;
+            double fees;
+
+            Console.Write("Enter Student Number: ");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter Student Name: ");
+            name = Console.ReadLine();
+
+            Console.Write("Enter Course: ");
+            course = Console.ReadLine();
+
+            Console.Write("Enter Semester: ");
+            semester = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter Fees: ");
+            fees = Convert.ToDouble(Console.ReadLine());
+
+            // Constructor
+            Student s1 = new Student(number, name, course, semester, fees);
+
+            // Scholarship Check
+            s1.Scholarship();
+
+            // Display Final Details
+            s1.Display();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PR2/PR2/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR2
+{
+    interface IPayroll
+    {
+        double CalculateSalary();
+    }
+
+    abstract class Employee : IPayroll
+    {
+        public int EmployeeId;
+        public string EmployeeName;
+        public double BasicSalary;
+
+        public Employee(int id, string name, double salary)
+        {
+            EmployeeId = id;
+            EmployeeName = name;
+            BasicSalary = salary;
+        }
+
+        public abstract double CalculateSalary();
+
+        public void Display(string type)
+        {
+            Console.WriteLine("\n------ Employee Details ------");
+            Console.WriteLine("Employee Type : " + type);
+            Console.WriteLine("Employee ID   : " + EmployeeId);
+            Console.WriteLine("Employee Name : " + EmployeeName);
+            Console.WriteLine("Basic Salary  : " + BasicSalary);
+            Console.WriteLine("HRA (10%)     : " + (BasicSalary * 0.10));
+            Console.WriteLine("DA (20%)      : " + (BasicSalary * 0.20));
+            Console.WriteLine("Gross Salary  : " + CalculateSalary());
+        }
+    }
+
+
+
+
+    class FullTimeEmployee : Employee
+    {
+        public FullTimeEmployee(int id, string name, double salary)
+            : base(id, name, salary)
+        {
+        }
+
+        public override double CalculateSalary()
+        {
+            return BasicSalary + (BasicSalary * 0.10);
+        }
+    }
+
+    class PartTimeEmployee : Employee
+    {
+        public PartTimeEmployee(int id, string name, double salary)
+            : base(id, name, salary)
+        {
+        }
+
+        public override double CalculateSalary()
+        {
+            return BasicSalary + (BasicSalary * 0.10);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("==================================");
+            Console.WriteLine(" Employee Payroll Management");
+            Console.WriteLine("==================================");
+
+            Console.WriteLine("\nSelect Employee Type");
+            Console.WriteLine("1. Full Time Employee");
+            Console.WriteLine("2. Part Time Employee");
+
+            Console.Write("\nEnter Choice : ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+
+
+            Employee emp;
+
+            if (choice == 1)
+            {
+
+                Console.Write("\nEnter Employee ID : ");
+                int id = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Enter Employee Name : ");
+                string name = Console.ReadLine();
+
+                Console.Write("Enter Salary : ");
+                double salary = Convert.ToDouble(Console.ReadLine());
+
+                emp = new FullTimeEmployee(id, name, salary);
+                emp.Display("Full Time");
+            }
+            else if (choice == 2)
+            {
+                Console.Write("\nEnter Employee ID : ");
+                int id = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Enter Employee Name : ");
+                string name = Console.ReadLine();
+
+                Console.Write("Enter Salary : ");
+                double salary = Convert.ToDouble(Console.ReadLine());
+
+                emp = new PartTimeEmployee(id, name, salary);
+                emp.Display("Part Time");
+            }
+            else
+            {
+                Console.WriteLine("Invalid ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+ }
